7c27fa3c3f91ecf91a5bfea6d9d48e31ddf6937b
LTDigor <71977735+LTDigor@users.noreply.github.com>
2020-10-12 22:26:57 +0300

    Update test_int.py

--- HW1/test_int.py
+++ HW1/test_int.py
@@ -25,7 +25,7 @@ class TestInt:
 
     # остаток от деления
     @pytest.mark.parametrize('a, i, expected', [(11, 1, 0), (11, 2, 1), (11, 11, 0), (11, 10, 1), (11, 12, 11)])
-    def test_int_mul(self, a, i, expected):
+    def test_int_div_remainder(self, a, i, expected):
         if i == 0:
             with pytest.raises(ZeroDivisionError):
                 assert a % i
